ad80f4e6f76aca95db74c229be6328be5b460f94
bryya <bryya@Bryans-Ghost>
2016-12-06 04:01:36 -0700

    fixed broken method call

--- src/MyTest.java
+++ src/MyTest.java
@@ -5,17 +5,18 @@ class MyTest {
 		NFLPlayer playerStats = new NFLPlayer();
 		
 		//Player1 finding quarterback rating
-		int touchdowns = 2;
-		int passingAttempts = 44;
-		int passingYards = 285;
-		int interceptions = 1;
-		int completedPasses = 35;
+		int touchdowns = 30;
+		int passingAttempts = 500;
+		int passingYards = 3913;
+		int interceptions = 11;
+		int completedPasses = 357;
 		
 		// Call QB rating method
 		playerStats.QBRating(touchdowns, passingAttempts, completedPasses,
 				passingYards, interceptions); 
 	
-		System.out.println(QBRating);
+		System.out.println(playerStats.QBRating(touchdowns, passingAttempts,
+				completedPasses, passingYards, interceptions));
 	}
 
 }
