8b14445f0b4ac8b8d34c393f91e7e360552ac7cd
bryya <bryya@Bryans-Ghost>
2017-01-08 23:36:24 -0700

    Application addition for main

---
+++ src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+	//Display list of players
+		 NFLPlayer playerStatistics = new NFLPlayer(player1.PlayerManager);
+
+	//Request next screen
+		 
+	//Display current roster
+	}
+
+}
